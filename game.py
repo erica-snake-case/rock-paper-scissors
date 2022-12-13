d07fd8ef53a12a79bbecde764854fe68ea47c75c
erica-snake-case <120158376+erica-snake-case@users.noreply.github.com>
2022-12-13 17:23:24 +0000

    Add game structure

---
+++ game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self, name1,  name2):
+        self.game_id = name1+name2 # TODO: implement better naming system
+        self.player1_name = name1 # TODO:  implement  Player class
+        self.player1_wins = 0
+        self.player2_name = "Computer" if name2 == "" else name2
+        self.player2_wins = 0
+        self.ties = 0
+
+
+
+
