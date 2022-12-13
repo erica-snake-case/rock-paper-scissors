62cfe2a7beff6e4ec00f07b1e5f468ed8ef03263
erica-snake-case <120158376+erica-snake-case@users.noreply.github.com>
2022-12-13 17:21:12 +0000

    add backend logic for game

---
+++ helpers.py
@@ -0,0 +1,39 @@
+SCORING = {"rock": {"beats": "scissors"},
+           "scissors": {"beats": "paper"},
+           "paper": {"beats": "rock"}}
+
+
+class InvalidInputError(Exception):
+    pass
+
+
+def beats(symbol1, symbol2):
+    try:
+        return SCORING[symbol1]["beats"] == symbol2
+    except KeyError:
+        raise InvalidInputError(f"must be rock, paper, or scissors, but entered {symbol1}")
+
+
+def get_result(symbol1, symbol2):
+    symbol1 = symbol1.lower()
+    symbol2 = symbol2.lower()
+    if beats(symbol1, symbol2):
+        return "player1_wins"
+    if beats(symbol2, symbol1):
+        return "player2_wins"
+    return "ties"
+
+
+# INFORMAL UNIT TESTING
+# print("expected = player1_wins")
+# print(get_result("scissors", "paper"))
+
+# print("expected = player2_wins")
+# print(get_result("rock", "paper"))
+
+# print("expected = ties")
+# print(get_result("paper", "paper"))
+
+# print("expected = InvalidInputError")
+# print(get_result("scissors", "watermelon"))
+# print(get_result("watermelon", "scissors"))
