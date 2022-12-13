1a3c2637bcc8c2c12bf6a32f7f1e7c921cc195e5
erica-snake-case <120158376+erica-snake-case@users.noreply.github.com>
2022-12-13 17:45:46 +0000

    add /new route

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask
+from game import Game
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+@app.route("/", methods = ['POST', 'GET'])
+def create_game():
+    if not request.args or request.args.get("player1") is None or request.args["player1"] == "":
+        # TODO: add error flashing
+        return render_template("new.html")
+    return f"{request.args['player1']} {request.args['player2']}"
+
